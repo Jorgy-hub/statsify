9d3ab7aa2209292c7c8eab4379ad0e15ccbcfa90
Uraraka-Chan <71365133+Uraraka-Chan@users.noreply.github.com>
2022-06-16 04:34:50 -0500

    fix: redirect fix

--- config_example.js
+++ config_example.js
@@ -1,6 +1,6 @@
 export const authEndpoint = "https://accounts.spotify.com/authorize";
 export const clientId = "48889495d12e4d27bd788c7f16b9a928";
-export const redirectUri = "http://localhost:3000";
+export const redirectUri = "http://75.119.136.254:3000";
 export const scopes = [
     "user-read-private",
     "user-top-read",

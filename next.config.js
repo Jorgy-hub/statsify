252ec35ad599f33a59b006b6baa51f3fa609cda8
Uraraka-Chan <71365133+Uraraka-Chan@users.noreply.github.com>
2022-06-16 03:58:58 -0500

    feat(release): first release

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  future: { webpack5: true },
+  future: { webpack5: false },
   distDir: 'build',
   images: {
-    domains: ["upload.wikimedia.org", "i.scdn.co"]
+    domains: ['upload.wikimedia.org', 'i.scdn.co'],
   }
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;

5f70298d880a1ff8c0ed480d0f667fba58bb74b0
Jorgy <71365133+Jorgy-hub@users.noreply.github.com>
2023-06-18 18:34:44 -0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -129,7 +129,6 @@ export default function Home() {
                   </div>
                 </div>
               </div>
-              <Credits />
             </div>
           )}
         </div>
